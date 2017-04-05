eb9de75f695044942bdc2d00f0837eb41dc67ed1
Nikhil Kumar <nikhilnickkumar@gmail.com>
2017-04-05 20:04:52 +0530

    Update and rename creation of bst using recursion.c to Creation of bst using recursion.c

---
+++ Binary Search Tree/C/Creation of bst using recursion.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct tree
+{
+	int data;
+	struct tree *left, *right;
+
+}tree;
+
+tree* newNode(int data)
+{
+	tree *nw = NULL;
+	
+	nw = (tree *)malloc(sizeof(tree));
+	nw->data = data;
+	nw->left = nw->right = NULL;
+	
+	return nw;
+}
+
+void createNode(tree **root, int data)
+{
+	if(*root == NULL)
+		*root = newNode(data);
+	else if(data <= (*root)->data)
+		createNode(&(*root)->left, data);
+	else
+		createNode(&(*root)->right, data);
+}
+
+void inorder(tree *root)
+{
+	if(root == NULL)
+		return;
+	inorder(root->left);
+	printf("%d ", root->data);
+	inorder(root->right);
+}
+ 
+int main(void)
+{
+	tree *t1 = NULL;
+	createNode(&t1, 20);
+	createNode(&t1, 15);
+	createNode(&t1, 25);
+	createNode(&t1, 12);
+	createNode(&t1, 18);
+	createNode(&t1, 22);
+	createNode(&t1, 28);
+	
+	inorder(t1);
+	
+	return 0;
+}
